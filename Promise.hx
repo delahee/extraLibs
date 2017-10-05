954af77bccf245f4441784cc767218c551ef5d46
David Elahee <david.elahee@gmail.com>
2017-10-05 12:40:27 +0200

    Better promises

--- Promise.hx
+++ Promise.hx
@@ -3,27 +3,80 @@ class Promise {
 	var thens:Array<Dynamic->Dynamic> = [];
 	var rejects:Array<Dynamic->Dynamic> = [];
 	
+	public var _succeeded = false;
+	public var _failed = false;
+	
+	public var curSuccess : Dynamic = null;
+	public var curFailure : Dynamic = null;
+	
 	public inline function then(f:Dynamic->Dynamic, ?r:Dynamic->Dynamic){
- 		thens.push( f );
-		if ( r != null) rejects.push(r);
+		if ( _succeeded ) { 
+			if ( f != null ) 
+				curFailure = f( curFailure );
+		}
+		else if ( _failed ){
+			if ( r != null ) 
+				curFailure = r( curFailure );
+		}
+		else {
+			thens.push( f );
+			if ( r != null) rejects.push(r);
+		}
 	}
 	
-	public inline function reject(f:Dynamic->Dynamic) 		rejects.push( f );
+	public inline function reject(f:Dynamic->Dynamic){
+		if (_failed){
+			curFailure = f( curFailure );
+		}
+ 		rejects.push( f );
+	}
 	
 	public inline function new() {}
 	
-	public function done(d : Dynamic ) {
+	public inline function resolve(?d : Dynamic ) {
+		done(d);
+	}
+	
+	public function done(?d : Dynamic ) {
+		_succeeded = true;
+		curSuccess = d;
 		for (s in thens) {
-			d = s(d);
+			curSuccess = s(curSuccess);
 		}
 		thens = [];
 		rejects = [];
+		return curSuccess;
 	}
 	
-	public function failed( d : Dynamic ) {
+	public function failed( ?d : Dynamic ) {
+		_failed = true;
+		curFailure = d;
 		for (s in rejects)
-			d = s(d);
+			curFailure = s(curFailure);
 		thens = [];
 		rejects = [];
+		return curFailure;
+	}
+	
+	public function chain( p : Promise ) : Promise{
+		p.then( done, failed );
+		return this;
+	}
+	
+	public static function test(){
+		var p = new Promise();
+		
+		var otherP = new Promise();
+		otherP.then( function(res){
+			trace("chained done:"+res);
+			return res;
+		}, function(res){
+			trace("chained err: "+res);
+			return res;
+		});
+		
+		otherP.chain( p );//alias p.then( other.done, other.failed )
+		
+		p.failed("err");
 	}
 }
